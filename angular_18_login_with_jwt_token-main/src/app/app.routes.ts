78b07f941cab987c7c870cc32c0d57c03a8cba8b
JuanSe <jsebastiannovoa@gmail.com>
2024-10-28 21:39:27 -0500

    Se agrega ayuda

--- angular_18_login_with_jwt_token-main/src/app/app.routes.ts
+++ angular_18_login_with_jwt_token-main/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/registerpage/register-page.component';
 import { ArrendadorComponent } from './pages/arrendador/arrendador.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/registerpage/register-page.component'; // Importa RegisterComponent
+// Importa RegisterComponent
 export const routes: Routes = [
   {
     path: '',
@@ -18,6 +20,10 @@ export const routes: Routes = [
   {
     path: 'buscar',
     component: BuscarComponent
+  },
+  {
+    path: 'ayuda',
+    component: AyudaComponent
   },
   {
     path: 'arrendador',
