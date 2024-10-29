066d8aa6b95afc7e46b58ffc994cc2271b95d597
Andres Centanaro <centanaro02@gmail.com>
2024-10-28 21:51:52 -0500

    se agrega el path de ayuda en routes

--- angular_18_login_with_jwt_token-main/src/app/app.routes.ts
+++ angular_18_login_with_jwt_token-main/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/registerpage/register-page.component';
 import { ArrendadorComponent } from './pages/arrendador/arrendador.component';
-import { AyudaComponent } from './pages/ayuda/ayuda.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/registerpage/register-page.component'; // Importa RegisterComponent
+import { RegisterComponent } from './auth/registerpage/register-page.component';
+import { PerfilArrendatarioComponent } from './pages//perfiles/perfil-arrendatario/perfil-arrendatario.component'; // Nueva importación
+import { PerfilArrendadorComponent } from './pages//perfiles/perfil-arrendador/perfil-arrendador.component'; // Nueva importación
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+
 export const routes: Routes = [
   {
     path: '',
@@ -19,12 +20,12 @@ export const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: 'buscar',
-    component: BuscarComponent
-  },
-  {
     path: 'ayuda',
     component: AyudaComponent
+  },
+  {
+    path: 'buscar',
+    component: BuscarComponent
   },
   {
     path: 'arrendador',
